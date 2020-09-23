4299e08bc77caf6998579fbbf232c5d352352458
Striker <zzstrikerzzyt@gmail.com>
2020-09-23 21:15:03 +0100

    Fix

--- commands/mute.js
+++ commands/mute.js
@@ -11,7 +11,7 @@ class MuteCommand extends Command {
     }
 
     async exec(msg) {
-        if (admins.indexOf(msg.user.id) > -1) {
+        if (admins.indexOf(msg.member.id) > -1) {
             msg.member.voice.channel.members.each(user => {
                 msg.guild.member(user).voice.setMute(true)
             })
